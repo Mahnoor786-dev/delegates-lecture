f33d9c2d757910204bf11dab1482e558c6bf72da
Mahnoor <mahnoorali57se@gmail.com>
2022-03-26 08:48:09 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Program
+{
+    delegate int MathOperation(int a, int b);
+    class MathOperation
+    {
+        public int Add(int a, int b)
+        {
+            Console.WriteLine("\nSum: "+ a + b);
+            return a+ b;
+        }
+        public int Subtract(int a, int b)
+        {
+            Console.WriteLine("\Difference: "+ a - b);
+            return a- b;
+        }
+        public int Multiply(int a, int b)
+        {
+            Console.WriteLine("\nProduct: "+ a + b);
+            return a* b;
+        }
+        public int Add(int a, int b)
+    }
+
+    public class HelloWorld
+    {
+
+
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
